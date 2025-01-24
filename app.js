577bc9ca9e90ea3fa264a41851a4a1208cf5b41d
devJudicael <Judicaelcakpo35@gmail.com>
2025-01-24 20:48:54 +0000

    fix

--- app.js
+++ app.js
@@ -22,11 +22,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-    exposedHeaders: ["set-cookie"],
+    origin: "http://localhost:5173", // L'origine autorisée (frontend)
+    credentials: true, // Permet les cookies ou les credentials
   })
 );
 
--- controllers/shopController.js
+++ controllers/shopController.js
@@ -4,6 +4,7 @@ const Product = require("../models/productModel");
 exports.getProducts = async (req, res) => {
   try {
     const products = await Product.find();
+
     res.json(products);
   } catch (error) {
     console.log(error);
